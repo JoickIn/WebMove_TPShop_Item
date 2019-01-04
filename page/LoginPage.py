506e94e2c3252a3cb77cbefcd1efdf63a177cbe7
kangkai <2366782068@qq.com>
2019-01-04 21:47:54 +0800

    第十次提交(登录页面显示密码功能)

--- page/LoginPage.py
+++ page/LoginPage.py
@@ -15,10 +15,12 @@ class LoginPage(BaseAction):
     password_input = By.ID, "com.tpshop.malls:id/edit_password"
     # (2.3): 登录按钮。
     login_button = By.ID, "com.tpshop.malls:id/btn_login"
+    # (2.4): 显示密码的按钮。然后在看下面的代码 (7)
+    show_password_button = By.ID, "com.tpshop.malls:id/img_view_pwd"
 
 
     # (4): 配置步骤。
-    @allure.step("登录页面-输入用户名")
+    @allure.step("登录页面-输入-用户名")
     # (3): 定位元素的动作函数。
     # (3.1): 输入用户名。
     def input_username(self, text):
@@ -27,7 +29,7 @@ class LoginPage(BaseAction):
         self.input(self.username_input, text)
 
     # (4.1): 配置步骤。
-    @allure.step("登录页面-输入密码")
+    @allure.step("登录页面-输入-密码")
     # (3.2): 输入密码。
     def input_password(self, text):
         # (5.1): 配置描述的信息。
@@ -35,7 +37,7 @@ class LoginPage(BaseAction):
         self.input(self.password_input, text)
 
     # (4.2): 配置步骤。
-    @allure.step("登录页面-点击登录按钮")
+    @allure.step("登录页面-点击-登录按钮")
     # (3.3): 点击登录按钮。
     def click_login(self):
         self.click(self.login_button)
@@ -55,4 +57,20 @@ class LoginPage(BaseAction):
         #     return True
         # else:
         #     return False
+
+
+    # (7.1): 配置中文步骤。
+    @allure.step("登录页面-点击-显示密码按钮")
+    # (7): 对显示按钮进行一个点击的操作。然后在回到 -> scripts 页面里面去调用他看 scripts 里面的 (10.5) 步骤
+    def click_show_password_button(self):
+        self.click(self.show_password_button)
+
+
+    # (8): 直接调用之前封装好 get_text() 函数即可,这个函数的意思就是说你给我这个函数一个特征那么我就帮助你返回
+    #      文字,所以我们在 scripts 里面去直接调用这个方法就可以了,看 test_login 里面的 (10.6)
+    def get_password_text(self):
+        return self.get_text(self.password_input)
+
+
+
 #——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————#
--- scripts/test_login.py
+++ scripts/test_login.py
@@ -27,32 +27,32 @@ class TestLogin:
 
 
     # (6): 配置参数化。在这个函数头上直接写即可。然后直接调用方法即可。看(6.1)
-    @pytest.mark.parametrize("args", analyze_file("test_login_data.yml", "test_login"))
+    # @pytest.mark.parametrize("args", analyze_file("test_login_data.yml", "test_login"))
     # (5): 测试用例: longin/(6.1): 加入形参 args 看(6.2)
-    def test_login(self, args):
+    # def test_login(self, args):
         # (6.2): 准备数据。看(6.3)
-        username = args['username']
-        password = args['password']
-        info = args['info']
+        # username = args['username']
+        # password = args['password']
+        # info = args['info']
 
 
         # (5.1): 点击我的。
-        self.Page.IndexPage.click_my_button()
+        # self.Page.IndexPage.click_my_button()
 
         # (5.2): 登录/注册。
-        self.Page.MyPage.click_login_and_sign_up_button()
+        # self.Page.MyPage.click_login_and_sign_up_button()
 
         # (5.3): 登录。
         # (5.3.1): 填写 username
         # self.Page.LoginPage.input_username('13800138006')
         # (6.4): 将字符串改为对应的数据变量。
-        self.Page.LoginPage.input_username(username)
+        # self.Page.LoginPage.input_username(username)
         # (5.3.2): 填写 password
         # self.Page.LoginPage.input_password('123456')
         # (6.4): 将字符串改为对应的数据变量。
-        self.Page.LoginPage.input_password(password)
+        # self.Page.LoginPage.input_password(password)
         # (5.3.3): 点击登录按钮
-        self.Page.LoginPage.click_login()
+        # self.Page.LoginPage.click_login()
         # (5.3.4): 判断关键字: 登录成功。基本判断,断言。
         # 解答: 如果登录成功等于 True 那么断言为 True 否则断言为 False 那么这个可以写为一行代码。看新的判断断言。
         # if self.Page.LoginPage.is_toast_keyword("登录成功") == True:
@@ -68,7 +68,7 @@ class TestLogin:
         #       的写法。
         # assert self.Page.LoginPage.is_toast_keyword("登录成功")
         # (6.4): 将字符串改为对应的数据变量。
-        assert self.Page.LoginPage.is_toast_keyword(info)
+        # assert self.Page.LoginPage.is_toast_keyword(info)
         #
         #
         # (5.3.4): 奇怪的写法。
@@ -80,23 +80,23 @@ class TestLogin:
 
     # (7): 第 2 套脚本设计 -> 只输入用户民或者密码。
     # (7.1): 定义第 2 套函数。
-    @pytest.mark.parametrize("args", analyze_file("test_login_data.yml", "test_login_miss_part"))
-    def test_login_miss_part(self, args):
+    # @pytest.mark.parametrize("args", analyze_file("test_login_data.yml", "test_login_miss_part"))
+    # def test_login_miss_part(self, args):
         # (7.2): 准备数据。
-        username = args['username']
-        password = args['password']
+        # username = args['username']
+        # password = args['password']
 
         # (7.3): 点击我的。
-        self.Page.IndexPage.click_my_button()
+        # self.Page.IndexPage.click_my_button()
 
         # (7.4): 登录/注册。
-        self.Page.MyPage.click_login_and_sign_up_button()
+        # self.Page.MyPage.click_login_and_sign_up_button()
 
         # (7.5): 输入用户名。
-        self.Page.LoginPage.input_username(username)
+        # self.Page.LoginPage.input_username(username)
 
         # (7.6): 输入密码。
-        self.Page.LoginPage.input_password(password)
+        # self.Page.LoginPage.input_password(password)
 
         # (8): 如果数据在设计的时候没有不输入的 key 那么就使用下面的全部的写法。或者直接用上面的 (7) 系列也是可以的。
         # if "username" in args:
@@ -109,10 +109,61 @@ class TestLogin:
 
 
         # (9): 断言,如果登录按钮可以点击,那么不通过,如果登录按钮不可以点击,那么就通过。
-        if self.Page.LoginPage.is_login_button_enabled() == True:
-            assert False    
+        # if self.Page.LoginPage.is_login_button_enabled() == True:
+        #     assert False
+        # else:
+        #     assert True
+
+
+
+
+    # (10): 测试显示密码。
+    def test_show_password(self):
+        # (10.7): 准备数据。
+        input_password = "111111"
+
+
+        # (10.1): 点击我的。
+        self.Page.IndexPage.click_my_button()
+
+
+        # (10.2): 点击 [登录/注册] 按钮。
+        self.Page.MyPage.click_login_and_sign_up_button()
+
+
+        # (10.3): 登录页面-输入密码。
+        # self.Page.LoginPage.input_password("111111")
+        #
+        # (10.3.1): ||||||升级的版本: 把原来的数据替换为变量。
+        self.Page.LoginPage.input_password(input_password)
+
+
+        # (10.4): 密码是不是没有显示(说白了在没点击显示按钮的时候密码是不是黑点的状态)。如果输入的密码前提是在没点这个显
+        #         示按钮之前就显示了密码的本体,那么就是说这个时候说明软件是有 bug 的。那么我们要做一个判断。 那么就是说
+        #         这个断言为 False 之后那么就是说后面的代码就不会去执行了。
+        # 解答: 那么就是说当这个 if 的条件如果是为真的时候,那么这个断言False他就会报错了,那么也就是说后面 else 里面的代
+        #       码是不会去执行的,那么如果说这个 if 判断条件他为 false 那么也不会进入到这个条件里面来而是直接就执行 else
+        #       里面的条件了。
+        if self.Page.LoginPage.get_password_text() == input_password:
+            assert False, "备注: password上的文字在没按显示按钮(默认的密码是黑点)的时候就显示了密码的实体,说明软件有 Bug。"
         else:
-            assert True
+            # (10.5): 点击显示密码的按钮(就是那个小眼睛的按钮)。
+            self.Page.LoginPage.click_show_password_button()
+
+            # (10.6): 断言: 密码框里面的文字是不是和之前输入的一致,如果是和之前输入的文字是一致的那么就没有 bug。那么我们要
+            #          封装到 base 里面一个函数,看 base 里面的 get_text() 这个封装好的函数,由于这个函数已经被封装好了, 那
+            #          么就是说我们在 LoginPage 里面直接去使用这个函数即可。 看 LoginPage 里面的 (8) 然后那么就是说他得到
+            #          的就是这 111111 那么我就要用这个函数去进行一个断言看看是不是 111111 那么由于这个几个 1 是重复的我们
+            #          可以放到一个变量里面去。看 (10.7)。
+            # assert "111111" == self.Page.LoginPage.get_password_text()
+            #
+            # (10.6.1): ||||||升级的版本: 把原来的数据替换为变量。
+            assert input_password == self.Page.LoginPage.get_password_text()
+
+
+
+
+
 
 
 
